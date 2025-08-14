d39e537106696cf924a620ec88ec812dbe0aa1c1
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-08-13 21:27:11 -0400

    Agragando busqueda en vivo

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -5,6 +5,8 @@ return {
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
       local telescope = require('telescope')
+            local builtin = require("telescope.builtin")
+            vim.keymap.set('n', '<C-f>', builtin.live_grep, {})
       telescope.setup({}) -- Use default Telescope settings
       -- Load the themes extension
       telescope.load_extension('themes')
@@ -14,5 +16,18 @@ return {
     -- This is the plugin that provides the theme chooser
     'andrewberty/telescope-themes',
     dependencies = { 'nvim-telescope/telescope.nvim' }
-  }
+  },
+    {
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			require("telescope").setup {
+  			extensions = {
+    				["ui-select"] = {
+				      require("telescope.themes").get_dropdown {}
+    					}
+	  			}
+			}
+			require("telescope").load_extension("ui-select")
+		end
+	}
 }

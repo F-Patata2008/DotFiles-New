85cf0e25d7bb58e4628684511b7fc79444bfefc9
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-08-17 22:52:33 -0400

    Fixed Formatter errors

--- nvim/.config/nvim/lua/plugins/formatter.lua
+++ nvim/.config/nvim/lua/plugins/formatter.lua
@@ -1,53 +1,49 @@
 return {
-  "mhartington/formatter.nvim",
-  -- Load the plugin when you open a file, which is early enough.
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    -- Your existing setup is correct.
-    -- This tells formatter.nvim which tools to use for which files.
-    require("formatter").setup({
-      logging = false,
-      filetype = {
-        lua = { require("formatter.defaults").stylua },
-        latex = { require("formatter.defaults").latexindent },
-        c = { require("formatter.defaults").clang_format },
-        cpp = { require("formatter.defaults").clang_format },
-        objc = { require("formatter.defaults").clang_format },
-        python = {
-          require("formatter.defaults").isort,
-          require("formatter.defaults").black,
-        },
-        javascript = { require("formatter.defaults").prettier },
-        typescript = { require("formatter.defaults").prettier },
-        javascriptreact = { require("formatter.defaults").prettier },
-        typescriptreact = { require("formatter.defaults").prettier },
-        html = { require("formatter.defaults").prettier },
-        css = { require("formatter.defaults").prettier },
-        scss = { require("formatter.defaults").prettier },
-        json = { require("formatter.defaults").prettier },
-        markdown = { require("formatter.defaults").prettier },
-        ["*"] = {
-          require("formatter.filetypes.any").remove_trailing_whitespace,
-        },
-      },
-    })
+    {
+      "mhartington/formatter.nvim",
+      -- Load the plugin when you open or create a file.
+      event = { "BufReadPre", "BufNewFile" },
+      config = function()
+        -- This setup function runs and registers the :Format command.
+        require("formatter").setup({
+          logging = false,
+          filetype = {
+            tex = { require("formatter.defaults").latexindent },
+            lua = { require("formatter.defaults").stylua },
+            c = { require("formatter.defaults").clang_format },
+            cpp = { require("formatter.defaults").clang_format },
+            objc = { require("formatter.defaults").clang_format },
+            python = {
+              require("formatter.defaults").isort,
+              require("formatter.defaults").black,
+            },
+            javascript = { require("formatter.defaults").prettier },
+            typescript = { require("formatter.defaults").prettier },
+            javascriptreact = { require("formatter.defaults").prettier },
+            typescriptreact = { require("formatter.defaults").prettier },
+            html = { require("formatter.defaults").prettier },
+            css = { require("formatter.defaults").prettier },
+            scss = { require("formatter.defaults").prettier },
+            json = { require("formatter.defaults").prettier },
+            markdown = { require("formatter.defaults").prettier },
+            ["*"] = {
+              require("formatter.filetypes.any").remove_trailing_whitespace,
+            },
+          },
+        })
 
-    -- KEY CHANGE: Use the standard Neovim formatting API.
-    -- This is more reliable than calling require("formatter").format() directly.
+        -- KEY CHANGE: Use the command, not the Lua function.
+        -- This is the stable way to call the formatter.
+        vim.keymap.set({ "n", "v" }, "<leader>gf", "<cmd>Format<cr>", { desc = "[G]o [F]ormat Buffer" })
 
-    -- Autocommand for format on save
-    local augroup = vim.api.nvim_create_augroup("FormatOnSave", { clear = true })
-    vim.api.nvim_create_autocmd("BufWritePre", {
-      group = augroup,
-      pattern = "*",
-      callback = function()
-        vim.lsp.buf.format({ async = true })
+        -- Use the plugin's recommended command for format-on-save.
+        local augroup = vim.api.nvim_create_augroup("FormatOnSave", { clear = true })
+        vim.api.nvim_create_autocmd("BufWritePre", {
+          group = augroup,
+          pattern = "*",
+          -- The :Format command will now work reliably here as well.
+          command = "Format",
+        })
       end,
-    })
-
-    -- Keymap for manual formatting
-    vim.keymap.set({ "n", "v" }, "<leader>gf", function()
-      vim.lsp.buf.format()
-    end, { desc = "[G]o [F]ormat Buffer" })
-  end,
+    },
 }

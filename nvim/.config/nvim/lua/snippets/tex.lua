107fa83f2ecaa66214c211cbc340f0f2037aeec0
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-08-24 10:46:57 -0400

    Agregando al snippet de latex soprte para derechos de autor

--- nvim/.config/nvim/lua/snippets/tex.lua
+++ nvim/.config/nvim/lua/snippets/tex.lua
@@ -4,7 +4,7 @@ local i = ls.insert_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 return {
-  -- Snippet 1: Basic LaTeX article in Spanish
+  -- Snippet 1: Basic LaTeX article in Spanish with Copyright
   s("Documento", fmt([[
 \documentclass[11pt]{{article}}
 \usepackage[spanish]{{babel}}
@@ -15,6 +15,24 @@ return {
 \usepackage[utf8]{{inputenc}}
 \usepackage[T1]{{fontenc}}
 \usepackage[margin=1in]{{geometry}}
+\usepackage{{fancyhdr}} % Para el pie de página personalizado
+\usepackage{{hyperref}} % Para metadatos y enlaces en el PDF
+
+% ----- CONFIGURACIÓN DE AUTORÍA Y PIE DE PÁGINA -----
+\hypersetup{{
+    pdftitle={{{}}},
+    pdfauthor={{{}}},
+    colorlinks=true,
+    linkcolor=black,
+    urlcolor=cyan
+}}
+
+% Configuración del pie de página con fancyhdr
+\pagestyle{{fancy}}
+\fancyhf{{}} % Limpia la cabecera y el pie de página actuales
+\fancyfoot[C]{{\footnotesize Felipe Colli, Todos los Derechos Reservados}} % Tu texto en el centro
+\fancyfoot[R]{{\thepage}} % Número de página a la derecha
+\renewcommand{{\headrulewidth}}{{0pt}} % Elimina la línea de la cabecera
 
 \title{{{}}}
 \author{{{}}}
@@ -23,13 +41,17 @@ return {
 \begin{{document}}
 
 \maketitle
+\thispagestyle{{fancy}} % Aplica el estilo de pie de página a la primera página también
 
 {}
 
 \end{{document}}
 ]], {
-    i(1, "Tu Título"),
-    i(2, "Tu Nombre"),
-    i(0, "Empieza a escribir aquí..."),
+    -- Nodos para fmt, en orden de aparición en el texto de arriba:
+    i(1, "Tu Título"),      -- 1. Usado para pdftitle
+    i(2, "Felipe Colli Olea"),      -- 2. Usado para pdfauthor
+    i(1, "Tu Título"),      -- 3. Usado para \title (reutiliza el nodo 1)
+    i(2, "Felipe Colli Olea"),      -- 4. Usado para \author (reutiliza el nodo 2)
+    i(0, "Empieza a escribir aquí..."), -- 5. Posición final del cursor
   })),
 }

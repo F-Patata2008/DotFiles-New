929f6188b226ab1123ba3cc8f8f855e79495e0e9
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-09-01 21:42:09 -0400

    Patata: 2025-09-01 21:42:09

--- hypr/.config/hypr/scripts/env.sh
+++ hypr/.config/hypr/scripts/env.sh
@@ -13,18 +13,12 @@ PROJECT_PATH="$1"
 
 # Main Code Window (nvim .): ~2/3 width, left side
 kitty --title code --class code_window --working-directory "$PROJECT_PATH" nvim . &
-sleep 0.2 # Give Kitty a moment to initialize for the rule to catch it
 
 # Input Window (nvim input): 1/4 of the remaining vertical space on the right
 kitty --title input --class input_window --working-directory "$PROJECT_PATH" nvim input &
-sleep 0.2
 
 # Compile Window (cd ...): next 1/4 vertical space
-# Using zsh -c to execute commands in a new zsh instance
 kitty --title compile --class compile_window --working-directory "$PROJECT_PATH" zsh -c "cd \"$PROJECT_PATH\"; zsh" &
-sleep 0.2
 
 # Exec Window (cd ...): remaining 1/2 vertical space
-# Using zsh -c to execute commands in a new zsh instance
 kitty --title exec --class exec_window --working-directory "$PROJECT_PATH" zsh -c "cd \"$PROJECT_PATH\"; zsh" &
-sleep 0.2

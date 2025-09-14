dbda0dd56289f5089378c41d85fad59e59f5f7c5
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-09-14 18:56:27 -0300

    Patata: 2025-09-14 18:56:25

--- hypr/.config/hypr/scripts/nigthligth.sh
+++ hypr/.config/hypr/scripts/nigthligth.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-if pgrep -x "hyprsunset" > /dev/null
-then
-    # If hyprsunset is running, kill it and notify
-    killall hyprsunset
-    notify-send "Night Light Disabled" -h string:x-canonical-private-synchronous:nightlight-toggle
+# This script toggles hyprsunset by checking its current state
+# NOTE: This requires `jq` to be installed (sudo pacman -S jq)
+
+CURRENT_STATE=$(hyprctl getoption misc:hyprsunset -j | jq -r '.str')
+
+if [[ "$CURRENT_STATE" == "on" ]]; then
+    hyprctl dispatch hyprsunset off
+    notify-send "Night Light Disabled"
 else
-    # If hyprsunset is not running, start it and notify
-    hyprsunset &
-    notify-send "Night Light Enabled" -h string:x-canonical-private-synchronous:nightlight-toggle
+    hyprctl dispatch hyprsunset on
+    notify-send "Night Light Enabled"
 fi

96777393cca0a7b30fda98f6a8d45b52fd92f752
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-10-01 16:45:39 -0300

    moviendo alias y shorcuts para q funionen

---
+++ zsh/custom/aliases.zsh
@@ -0,0 +1,20 @@
+alias nv='nvim'
+
+
+alias clases='cd ~/Apunte/'
+alias PE='nvim ~/Apunte/Probabilidad\ y\ Estadistica/Apunte/Apunte.tex'
+alias prob='cd ~/Apunte//Probabilidad\ y\ Estadistica/'
+
+
+alias please='sudo $(fc -ln -1)'
+alias update='./~/Dotfiles/hypr/.config/hypr/scripts/update.sh'
+
+alias 'exec zsh'='clear; exec zsh'
+alias tl='java -jar ~/Desktop/TLauncher.v16/TLauncher.jar'
+alias cl='clear; tree'
+alias arduino='sudo chmod a+rw /dev/ttyACM0'
+
+
+alias wifi='nmcli dev wifi list --rescan yes'
+alias celu='nmcli dev wifi connect "Doxiado" --ask'
+alias papo="nmcli dev wifi connect \"Alvaro's A53\" --ask"
---
+++ zsh/custom/shorcuts.zsh
@@ -0,0 +1,13 @@
+#Atajos Probabilidad y Estadistica
+Apunte=~/Clases/Probabilidad\ y\ Estadistica/Apunte/
+Trabajo=~/Clases/Probabilidad\ y\ Estadistica/Trabajos/
+Proyecto=~/Clases/Probabilidad\ y\ Estadistica/
+
+#Atajo ZSH
+bash=~/.oh-my-zsh/custom/
+
+#Atajos Prog
+C=~/Progra/CIPC/
+#Java=~/Prog/Java/
+Python=~/Progra/Python/
+arduino=~/Arduino/

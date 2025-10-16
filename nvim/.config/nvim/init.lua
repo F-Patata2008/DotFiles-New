88ccb9ffa67217f82032e8a63df14facb9152f27
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-10-16 15:18:05 -0300

    Sigueindo lo que dice mejor

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -25,3 +25,13 @@ require("core.autostart")
 require("core.keybinds")
 
 
+-- Load LSP configuration first
+require("Arduino-Nvim.lsp").setup()
+
+-- Set up Arduino file type detection
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "arduino",
+    callback = function()
+        require("Arduino-Nvim")
+    end
+})

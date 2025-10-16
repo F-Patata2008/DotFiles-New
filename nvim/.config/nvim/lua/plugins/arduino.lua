502167ad14b77e1f8eaab6bdb02822f95d20dd61
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-10-16 14:56:10 -0300

    Eliminaod arduino.lua

--- nvim/.config/nvim/lua/plugins/arduino.lua
+++ nvim/.config/nvim/lua/plugins/arduino.lua
@@ -1,18 +1,3 @@
--- lua/plugins/arduino.lua
 return {
-  {
-    "yuukiflow/Arduino-Nvim",
-    ft = { "ino" },
-    dependencies = {
-      "nvim-telescope/telescope.nvim",
-    },
-    config = function()
-      require("Arduino-Nvim.lsp").setup()
 
-      require("Arduino-Nvim").setup({
-        default_fqbn = "arduino:avr:uno",
-        -- default_port = "/dev/ttyUSB0",
-      })
-    end,
-  }
 }

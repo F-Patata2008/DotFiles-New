deb5bd95c5058e9b8c0267b1504d60beb7feb40c
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-10-16 15:12:57 -0300

    toy medio chato ya

--- nvim/.config/nvim/lua/plugins/arduino.lua
+++ nvim/.config/nvim/lua/plugins/arduino.lua
@@ -1,11 +1,3 @@
 return {
-  "yuukiflow/Arduino-Nvim",
-  ft = "arduino",
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
-  },
-  config = function()
-    require("Arduino-Nvim.lsp").setup()
-    require("Arduino-Nvim").setup()
-  end,
+
 }

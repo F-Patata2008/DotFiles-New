0e45cdfd37ce22e5e699b5d75b8121185c7ee9c3
F-Patata2008 <felipe.colli.scratch@gmail.com>
2025-11-01 22:43:24 -0300

    ehhh, creoq  configuarndo weas de bat

--- zsh/.zsh/custom/aliases.zsh
+++ zsh/.zsh/custom/aliases.zsh
@@ -18,3 +18,8 @@ alias arduino='sudo chmod a+rw /dev/ttyACM0'
 alias wifi='nmcli dev wifi list --rescan yes'
 alias celu='nmcli dev wifi connect "Doxiado" --ask'
 alias papo="nmcli dev wifi connect \"Alvaro's A53\" --ask"
+
+# My custom power profiles for TLP
+alias set-powersave="echo powersave | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor && echo power | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference"
+alias set-balanced="echo schedutil | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor && echo balance_power | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference"
+alias set-performance="echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/scaling_governor && echo performance | sudo tee /sys/devices/system/cpu/cpu*/cpufreq/energy_performance_preference"

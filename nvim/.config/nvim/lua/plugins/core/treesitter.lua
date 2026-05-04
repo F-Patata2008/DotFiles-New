516ffbcbb912ee25cdbb17f2747ec63a7cd7172a
F-Patata2008 <felipe.colli.scratch@gmail.com>
2026-05-03 23:48:44 -0400

    Arregalnfo treesiter

--- nvim/.config/nvim/lua/plugins/core/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/core/treesitter.lua
@@ -2,30 +2,29 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    opts = function(_, opts)
-      -- add custom parser
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-      parser_config.hyprlang = {
-        install_info = {
-          url = "https://github.com/tree-sitter-grammars/tree-sitter-hyprlang",
-          files = { "src/parser.c" },
-          branch = "main",
+    opts = {
+      -- custom parser (new-style)
+      parser_config = {
+        hyprlang = {
+          install_info = {
+            url = "https://github.com/tree-sitter-grammars/tree-sitter-hyprlang",
+            files = { "src/parser.c" },
+            branch = "main",
+          },
+          filetype = "hyprlang",
         },
-        filetype = "hyprlang",
-      }
+      },
 
-      -- keep/merge your settings
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, {
+      ensure_installed = {
         "c", "lua", "vim", "vimdoc", "cpp", "hyprlang", "latex",
         "markdown", "markdown_inline", "html", "css", "scss", "json",
         "bash", "javascript",
-      })
+      },
 
-      opts.sync_install = false
-      opts.auto_install = true
-      opts.highlight = { enable = true }
-      opts.indent = { enable = true }
-    end,
+      sync_install = false,
+      auto_install = true,
+      highlight = { enable = true },
+      indent = { enable = true },
+    },
   },
 }

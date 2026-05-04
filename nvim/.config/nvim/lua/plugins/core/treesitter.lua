ba194d90b682ee05ce2a83b85f4a41d8def8450f
F-Patata2008 <felipe.colli.scratch@gmail.com>
2026-05-03 23:42:40 -0400

    intentando arregalr nvim

--- nvim/.config/nvim/lua/plugins/core/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/core/treesitter.lua
@@ -2,9 +2,9 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    config = function()
+    opts = function(_, opts)
+      -- add custom parser
       local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
       parser_config.hyprlang = {
         install_info = {
           url = "https://github.com/tree-sitter-grammars/tree-sitter-hyprlang",
@@ -14,32 +14,30 @@ return {
         filetype = "hyprlang",
       }
 
-      require('nvim-treesitter.configs').setup({
-        ensure_installed = { 
-                    "c",
-                    "lua", 
-                    "vim", 
-                    "vimdoc", 
-                    "cpp", 
-                    "hyprlang", 
-                    "latex", 
-                    "markdown", 
-                    "markdown_inline",
-                    "html",
-                    "css",
-                    "scss",
-                    "json",
-                    "bash",
-                    "javascript",
-                },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-        },
-        indent = { enable = true },
+      -- keep/merge your settings
+      opts.ensure_installed = opts.ensure_installed or {}
+      vim.list_extend(opts.ensure_installed, {
+        "c",
+        "lua",
+        "vim",
+        "vimdoc",
+        "cpp",
+        "hyprlang",
+        "latex",
+        "markdown",
+        "markdown_inline",
+        "html",
+        "css",
+        "scss",
+        "json",
+        "bash",
+        "javascript",
       })
+
+      opts.sync_install = false
+      opts.auto_install = true
+      opts.highlight = { enable = true }
+      opts.indent = { enable = true }
     end,
   },
 }
-

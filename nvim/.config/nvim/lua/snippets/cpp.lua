faef89f207da683c325456b0b9975b29568e6220
F-Patata2008 <felipe.colli.scratch@gmail.com>
2026-07-22 22:27:57 -0400

    feuture: agrgando snippet para prefix function (porgcomp)

--- nvim/.config/nvim/lua/snippets/cpp.lua
+++ nvim/.config/nvim/lua/snippets/cpp.lua
@@ -102,6 +102,30 @@ return {
   ),
 
   s(
+  "kmp",
+  {
+    t({
+      "vector<ll> prefix_function(const string &s) {",
+      "    int n = s.size();",
+      "    vector<ll> pi(n, 0);",
+      "    for (int i = 1; i < n; i++) {",
+      "        int j = pi[ i - 1 ];",
+      "        while (j > 0 && s[ i ] != s[ j ])",
+      "            j = pi[ j - 1 ];",
+      "        if (s[ i ] == s[ j ])",
+      "            j++;",
+      "        pi[ i ] = j;",
+      "    }",
+      "    return pi;",
+      "}",
+    }),
+  },
+  {
+    description = "KMP Prefix Function",
+  }
+),
+
+  s(
     "for",
     {
       t("for (int i = 0; i < n; i++) {"),
